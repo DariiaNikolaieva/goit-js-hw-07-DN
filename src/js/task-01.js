76deffaadd11f06d86cf637b642b349c2234864f
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-04-18 21:23:30 +0300

    Task 1, 2

---
+++ src/js/task-01.js
@@ -0,0 +1,40 @@
+/*Напиши скрипт, который выполнит следующие операции.
+
+Посчитает и выведет в консоль количество категорий в ul#categories, 
+то есть элементов li.item. Получится 'В списке 3 категории.'.
+
+Для каждого элемента li.item в списке ul#categories, найдет 
+и выведет в консоль текст заголовка элемента (тега h2) 
+и количество элементов в категории (всех вложенных в него элементов li).
+
+Например для первой категории получится:
+
+Категория: Животные
+Количество элементов: 4
+*/
+
+const countQuantityOfCategories = (categories) => {
+  return `В списке ${categories.children.length} категории.`;
+};
+
+const getTitleOfCategoryItem = (index) => {
+  const titleArray = document.querySelectorAll("h2");
+  return `Категория: ${titleArray[index].textContent}`;
+};
+
+const getQuantityOfItemEl = (index) => {
+  const itemEl = document.querySelectorAll(".item");
+  const quantityOfItems = itemEl[index].querySelectorAll("li");
+  return `Количество элементов: ${quantityOfItems.length}`;
+};
+
+console.log(countQuantityOfCategories(document.querySelector("ul#categories")));
+
+console.log(getTitleOfCategoryItem(0));
+console.log(getQuantityOfItemEl(0));
+
+console.log(getTitleOfCategoryItem(1));
+console.log(getQuantityOfItemEl(1));
+
+console.log(getTitleOfCategoryItem(2));
+console.log(getQuantityOfItemEl(2));
---
+++ src/js/task-02.js
@@ -0,0 +1,19 @@
+/*Напиши скрипт, который для каждого элемента массива ingredients 
+создаст отдельный li, 
+после чего вставит все li за одну операцию в список ul.ingredients. 
+Для создания DOM-узлов используй document.createElement().*/
+
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
+
+ingredients.forEach((ingredient) => {
+  const ingredientsItems = document.createElement("li");
+  ingredientsItems.textContent = `${ingredient}`;
+  document.getElementById("ingredients").append(ingredientsItems);
+});
